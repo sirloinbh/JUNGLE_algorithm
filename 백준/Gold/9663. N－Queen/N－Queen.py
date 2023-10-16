bc36c8f686a61a358c101eb555e127f8f04584f6
SEB_FE_45_김병현 <129952654+sirloinbh@users.noreply.github.com>
2023-10-16 14:55:14 +0900

    [Gold IV] Title: N-Queen, Time: 11412 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,26 +1,15 @@
-def is_available(current_col):
-    current_row = len(candidate)
-    for queen_row in range(current_row):
-        # 같은 열에 있거나 대각선에 위치하는지 확인
-        if candidate[queen_row] == current_col or \
-            candidate[queen_row] - queen_row == current_col - current_row or \
-            candidate[queen_row] + queen_row == current_col + current_row:
-            return False
-    return True
-
-def DFS(N, current_row):
+def solve(row, left, right, vertical):
     global result
-    if current_row == N:
-        result += 1
-        return
-    for candidate_col in range(N):
-        if is_available(candidate_col):
-            candidate.append(candidate_col)
-            DFS(N, current_row + 1)
-            candidate.pop()
+    available = ((1 << N) - 1) & ~(left | right | vertical)
+    while available:
+        lsb = -available & available
+        available -= lsb
+        if row == N - 1:
+            result += 1
+        else:
+            solve(row + 1, (left | lsb) << 1, (right | lsb) >> 1, vertical | lsb)
 
 N = int(input())
 result = 0
-candidate = []
-DFS(N, 0)
+solve(0, 0, 0, 0)
 print(result)

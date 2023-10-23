2e9a049ef953c0242d8561a28287f3a44b28ed91
SEB_FE_45_김병현 <129952654+sirloinbh@users.noreply.github.com>
2023-10-23 16:35:49 +0900

    [Gold III] Title: 아침 산책, Time: 916 ms, Memory: 97708 KB, Score: 108 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,41 +1,44 @@
 import sys
+from collections import deque
 
-def DFS(graph,start,A):
-    visited=[]
-    stack =[start]
-    cnt=0
-    
-    if A[start-1]==0:
-        return cnt 
-    else:
-        while stack:
-            vertex=stack.pop()
-            
-            if vertex not in visited:
-                if A[vertex-1]==0 or vertex==start:
-                    visited.append(vertex)
-                    stack.extend(sorted(graph[vertex], reverse =True))
-                else:
-                    visited.append(vertex)
-                    cnt+=1
-                    
+def BFS(graph, start, A):
+    visited = set()
+    queue = deque([start])
+    cnt = 0
+
+    if A[start-1] == 0:
         return cnt
-        
+
+    while queue:
+        vertex = queue.popleft()
+
+        if vertex not in visited:
+            visited.add(vertex)
+
+            if A[vertex-1] == 1 and vertex != start:
+                cnt += 1
+            else:
+                for neighbor in graph[vertex]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+
+    return cnt
 
 def main():
-    N= int(sys.stdin.readline())
-    A= sys.stdin.readline().strip()
-    A = [int(digit) for digit in str(A)]
+    N = int(sys.stdin.readline())
+    A = [int(digit) for digit in sys.stdin.readline().strip()]
     
-    graph = {i:[] for i in range(1,N+1)}
-    cnt=0
+    graph = {i: [] for i in range(1, N+1)}
+    cnt = 0
     
     for _ in range(N-1):
         u, v = map(int, sys.stdin.readline().split())
         graph[u].append(v)
         graph[v].append(u)
-    
-    for i in range(1,N+1):
-        cnt+=DFS(graph,i,A)
+
+    for i in range(1, N+1):
+        cnt += BFS(graph, i, A)
+
     print(cnt)
+
 main()

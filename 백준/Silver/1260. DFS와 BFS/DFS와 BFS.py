0cea4d3a8b3c8fedbbb6ec9011aafb0bc9e09337
SEB_FE_45_김병현 <129952654+sirloinbh@users.noreply.github.com>
2023-10-23 11:25:36 +0900

    [Silver II] Title: DFS와 BFS, Time: 276 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,46 +1,43 @@
 from collections import deque
+import sys
 
-def dfs(graph, start):
-    visited = []
-    stack = [start]
-
-    while stack:
-        vertex = stack.pop()
-        if vertex not in visited:
-            visited.append(vertex)
-            # 인접한 정점들을 스택에 추가합니다.
-            # 여기서 역순으로 추가하는 이유는 작은 번호의 정점부터 방문하기 위함입니다.
-            stack.extend(sorted(graph[vertex], reverse=True))
-    return visited
-
-def bfs(graph, start):
-    visited = []
+def BFS(graph,start):
+    visited=[]
     queue = deque([start])
 
+    
     while queue:
-        vertex = queue.popleft()
+        vertex=queue.popleft()
         if vertex not in visited:
             visited.append(vertex)
-            # 인접한 정점들을 큐에 추가합니다.
             queue.extend(sorted(graph[vertex]))
     return visited
 
-def dfs_bfs_with_io():
-    # 정점의 수, 간선의 수, 시작 정점 번호를 입력받습니다.
-    N, M, V = map(int, input().split())
-    
-    # 그래프를 초기화합니다.
-    graph = {i: [] for i in range(1, N + 1)}
+                
+def DFS(graph,start):
+    visited=[]
+    stack =[start]
 
-    # 간선 정보를 입력받아 그래프를 구성합니다.
+    while stack:
+        vertex=stack.pop()
+        if vertex not in visited:
+            visited.append(vertex)
+            stack.extend(sorted(graph[vertex], reverse =True))
+    return visited
+
+
+
+def main():
+    N, M,V = map(int, sys.stdin.readline().split())
+
+    
+    graph = {i:[] for i in range(1,N+1)}
+    
     for _ in range(M):
-        u, v = map(int, input().split())
+        u, v = map(int, sys.stdin.readline().split())
         graph[u].append(v)
         graph[v].append(u)
-
-    # DFS와 BFS 결과를 출력합니다.
-    print(" ".join(map(str, dfs(graph, V))))
-    print(" ".join(map(str, bfs(graph, V))))
-
-# 주석 처리하여 실행 오류를 막습니다.
-dfs_bfs_with_io()
+    print(" ".join(map(str,DFS(graph,V))))
+    print(" ".join(map(str,BFS(graph,V))))
+    
+main()

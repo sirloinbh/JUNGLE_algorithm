e11c673e8a924f99fe20620e549ee15b859a19d6
SEB_FE_45_김병현 <129952654+sirloinbh@users.noreply.github.com>
2023-12-28 19:06:49 +0900

    [Bronze III] Title: 사분면, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9610. 사분면/사분면.py
+++ 백준/Bronze/9610. 사분면/사분면.py
@@ -1,18 +1,19 @@
 N = int(input())
-result = [0 for _ in range(6)]
+result = [0, 0, 0, 0, 0]
 
-for _ in range(N) :
+for _ in range(N):
     x, y = map(int, input().split())
-    if x >0 :
-        if y>0 : result[1]+=1
-        elif y<0 : result[4] += 1
-        else : result[5] += 1
-    elif x<0 :
-        if y>0 : result[2]+=1
-        elif y < 0 : result[3] += 1
-        else :result[5] += 1
-    else : result[5] +=1
+    if x == 0 or y == 0:
+        result[0] += 1
+    elif x > 0 and y > 0:
+        result[1] += 1
+    elif x < 0 and y > 0:
+        result[2] += 1
+    elif x < 0 and y < 0:
+        result[3] += 1
+    elif x > 0 and y < 0:
+        result[4] += 1
 
-for i in range(1,len(result)-1) :
-    print("Q%d: %d" %(i,result[i]))
-print("AXIS: %d" %(result[-1]))
+for i in range(1, 5):
+    print(f"Q{i}: {result[i]}")
+print(f"AXIS: {result[0]}")

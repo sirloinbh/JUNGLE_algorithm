da9650838a7f6ffe026324e6f878b3456860117c
SEB_FE_45_김병현 <129952654+sirloinbh@users.noreply.github.com>
2023-12-29 13:04:35 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.py
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.py
@@ -1,6 +1,3 @@
-# 입력: 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
 n = int(input())
-
-# 출력: 첫째 줄부터 N번째 줄까지 차례로 별을 출력한다.
 for i in range(n, 0, -1):
-    print(' ' * (n - i) + '*' * i)
+    print(('*' * i).rjust(n))

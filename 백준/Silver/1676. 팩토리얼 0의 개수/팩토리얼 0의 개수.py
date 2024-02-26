4cdc01c43727a7e7cd432c6a9c599f35b955418b
SEB_FE_45_김병현 <129952654+sirloinbh@users.noreply.github.com>
2024-02-26 21:44:13 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,11 +1,10 @@
+N = int(input())
 zero_num = 0
 
-N = int(input())
-for i in range(1,N+1) :
-    if i%125 == 0 :
-        zero_num += 3
-    elif i%25 == 0 :
-        zero_num += 2
-    elif i%5 == 0 :
-        zero_num += 1
+# 5의 거듭제곱으로 나누기
+power_of_5 = 5
+while N >= power_of_5:
+    zero_num += N // power_of_5
+    power_of_5 *= 5
+
 print(zero_num)

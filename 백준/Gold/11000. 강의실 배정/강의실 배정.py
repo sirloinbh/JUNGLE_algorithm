e695626b4af92f2966bdec625c45a214200fd39a
SEB_FE_45_김병현 <129952654+sirloinbh@users.noreply.github.com>
2024-02-29 10:32:20 +0900

    [Gold V] Title: 강의실 배정, Time: 368 ms, Memory: 74228 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -3,15 +3,15 @@ import sys
 
 N = int(sys.stdin.readline())
 classes = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-classes.sort(key=lambda x: x[0])  # 시작 시간을 기준으로 정렬
+classes.sort(key=lambda x: x[0])
 
-rooms = []  # 각 강의실의 마지막 수업의 종료 시간을 저장할 힙
-heapq.heappush(rooms, classes[0][1])  # 첫 번째 수업의 종료 시간을 힙에 추가
+rooms = [] 
+heapq.heappush(rooms, classes[0][1])
 
 for i in range(1, N):
-    if rooms[0] <= classes[i][0]:  # 가장 빨리 끝나는 강의실의 마지막 수업이 현재 수업 시작 전에 끝나면
-        heapq.heappop(rooms)  # 해당 강의실에서 현재 수업을 진행
+    if rooms[0] <= classes[i][0]: 
+        heapq.heappop(rooms) 
 
-    heapq.heappush(rooms, classes[i][1])  # 현재 수업의 종료 시간을 힙에 추가
+    heapq.heappush(rooms, classes[i][1])
 
-print(len(rooms))  # 필요한 강의실의 수 출력
+print(len(rooms))

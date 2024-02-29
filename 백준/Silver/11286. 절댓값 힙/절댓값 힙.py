bf595b8956e7ea9e79652ba64ae639b26cfe1059
SEB_FE_45_김병현 <129952654+sirloinbh@users.noreply.github.com>
2024-02-29 11:29:36 +0900

    [Silver I] Title: 절댓값 힙, Time: 144 ms, Memory: 39824 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+
+N = int(sys.stdin.readline())
+heap = []
+
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, (abs(x), x))
